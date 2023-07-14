1acacde822dd6441b288fb0ecf6c4fdd02c30bbf
bazant23 <120146442+bazant23@users.noreply.github.com>
2023-07-14 20:33:39 -0300

    atualização por parte do jarvascript

---
+++ script.js
@@ -0,0 +1,8 @@
+const form = document.querySelector ("#form")
+const nameInput = document.queryselector ("#name")
+const emailInput  = document.queryselector ("#email")
+const messageTextarea = document.queryselector ("#message")
+
+ form.addEventListener("submit", (Event) => {
+    Event.preventDefault()
+ })
