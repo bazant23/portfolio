d298875191a7cf42c381a3fddd687ec5a24a7322
bazant23 <120146442+bazant23@users.noreply.github.com>
2023-07-17 18:51:01 -0300

    fazendo alguns ajustes

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-const form = document.querySelector ("#form")
-const nameInput = document.querySelector ("#name")
-const emailInput  = document.querySelector ("#email")
-const messageTextarea = document.querySelector ("#message")
+const form = document.querySelector ("#form");
+const nameInput = document.querySelector ("#name");
+const emailInput  = document.querySelector ("#email");
+const messageTextarea = document.querySelector ("#message");
+
 
  form.addEventListener("submit", (event) => {
     Event.preventDefault();
     //esse comando é para que seja colocado um alerta dizendo para colocar o nome da pessoa que está entrando em contato
 
     if (nameInput.value === "") {
-      alert("Por favor, preencha seu Nome Completo");
+      alert("Por favor, preencha o seu nome!");
       return;
+
     }
 
     if (emailInput.value === "") {
-      alert("Por favor,coloque seu email");
+      alert("Por favor,coloque seu email!");
       return;
     }
     
+    if (messageTextarea ==="")
     //Se todos os campos forem corretamente preenchidos , enviar o form
-    form.submit();
+  form.submit();
  });
