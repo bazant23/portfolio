756ae0586e96f42f2504540d0ccd6752163c9457
bazant23 <120146442+bazant23@users.noreply.github.com>
2023-09-17 20:47:24 -0300

    aprendendo o if e else

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
-let num1 = 10
-let num2 = 5
-let num3 = 10
+let nota1 = Number(prompt("Digite sua nota da primeira Unidade:"));
+let nota2 = Number(prompt("Digite a sua segunda unidade:"));
 
-console.log(num1>num2);
+let resultado = (nota1 + nota2) /2;
+
+alert(resultado >=5);
+
+if(resultado >=5){
+    alert("Parabéns, você passou de Ano :) ")
+}else{
+    alert("Infelizmente você não tirou a média, você está em recuperação :/")
+}
