cae5b15519780e445ce38520081464212a0625ff
bazant23 <120146442+bazant23@users.noreply.github.com>
2023-09-19 17:00:33 -0300

    alterações

--- script.js
+++ script.js
@@ -1,12 +1,7 @@
-let nota1 = Number(prompt("Digite sua nota da primeira Unidade:"));
-let nota2 = Number(prompt("Digite a sua segunda unidade:"));
+let notas = [2, 4, 5, 6, 7, 8, 3, 2, 1,10];
+let total = 0;
 
-let resultado = (nota1 + nota2) /2;
-
-alert(resultado >=5);
-
-if(resultado >=5){
-    alert("Parabéns, você passou de Ano :) ")
-}else{
-    alert("Infelizmente você não tirou a média, você está em recuperação :/")
-}
+for (let repetições = 0; repetições < notas.length; repetições++) {
+    total += notas[repetições]
+};
+console.log("O tatal do seu dinheiro é:" + (total));
